7b6176e17f0bca0d0375df9dd0a33ac6767bbb64
Abdullah Khan <khanabdullah080@gmail.com>
2022-08-16 21:34:34 +1000

    Update Container.js
    Added head component

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,8 +1,15 @@
+import Head from "next/head";
+
 const Container = ({ children }) => {
   return (
-    <main className="  bg-zinc-50 dark:bg-zinc-900 min-h-screen pt-20 pb-20">
-      <div className="max-w-4xl mx-auto p-4 md:p-6">{children}</div>
-    </main>
+    <>
+      <Head>
+        <title>Vercel Assessment</title>
+      </Head>
+      <main className="  bg-zinc-50 dark:bg-zinc-900 min-h-screen pt-20 pb-20">
+        <div className="max-w-4xl mx-auto p-4 md:p-6">{children}</div>
+      </main>
+    </>
   );
 };
 export default Container;
